340a05eda4527a4e36bd515492501a5e55ad0e78
jpaulobf <jpaulobf@gmail.com>
2024-05-25 21:58:55 +0100

    WIP

--- Spark-Kafka/python/sparkmain.py
+++ Spark-Kafka/python/sparkmain.py
@@ -1,8 +1,17 @@
 import os
+import struct
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField,FloatType,IntegerType,StringType,TimestampType
-from pyspark.sql.functions import from_json,col
+from pyspark.sql.functions import from_json,col, to_json
+from struct import Struct
+
+def writeToKafka(writeDF, _):
+    writeDF.write \
+                .format("kafka")\
+                .option("kafka.bootstrap.servers", "172.17.0.1:9092") \
+                .option("topic", "sales-transactions-2") \
+                .save()
 
 transactionSchema = StructType([
                         StructField("transactionId",StringType(),False),
@@ -24,9 +33,12 @@ spark = SparkSession \
         .master("local") \
         .config("spark.streaming.stopGracefullyOnShutdown", "true")\
         .config("spark.driver.host", "localhost")\
-        .config("spark.jars", os.getcwd() + "/jars/spark-sql-kafka-0-10_2.12-3.0.0.jar"+ "," + os.getcwd() + "/jars/kafka-clients-3.0.0.jar") \
+        .config("spark.jars", os.getcwd() + "/jars/spark-sql-kafka-0-10_2.12-3.0.0.jar" + "," + 
+                              os.getcwd() + "/jars/kafka-clients-3.0.0.jar" + "," +
+                              os.getcwd() + "/jars/spark-streaming-kafka-0-10_2.12-3.0.0.jar" + "," +
+                              os.getcwd() + "/jars/commons-pool2-2.8.0.jar" + "," +
+                              os.getcwd() + "/jars/spark-token-provider-kafka-0-10_2.12-3.0.0.jar") \
         .getOrCreate()
-
 
 spark.sparkContext.setLogLevel("ERROR")
 
@@ -38,10 +50,33 @@ df = spark \
         .option("startingOffsets", "earliest") \
         .load() 
 
-df.printSchema()
-df1 = df.select(from_json(col("value").cast("string"),transactionSchema).alias("data"))
-df1.printSchema()
+df1 = df.select(from_json(col("value").cast("string"),transactionSchema).alias("value"))
 
-df_exp = df1.select("data.transactionId", "data.productId")
+df_exp = df1.select("value.transactionId", 
+                    "value.productId", 
+                    "value.productName", 
+                    "value.productCategory", 
+                    "value.productPrice", 
+                    "value.productQuantity", 
+                    "value.productBrand", 
+                    "value.currency",
+                    "value.customerId",
+                    "value.transactionDate",
+                    "value.paymentMethod")
+
 df_exp.printSchema()
+
+#.foreachBatch(writeToKafka) \
+
+df_exp.writeStream \
+        .format("json") \
+        .queryName("Q Name") \
+        .outputMode("append") \
+        .option("path", "output") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .trigger(processingTime="5 second") \
+        .start()\
+        .awaitTermination()
+
+df_exp.show()
 
